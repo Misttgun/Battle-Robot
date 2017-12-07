72ba2b71293eabeb387b83ffcc946e260658aae7
misttgun <maurelarchange@gmail.com>
2017-12-07 23:26:02 +0100

    Création de l'UI pour le lobby

--- Unity/Assets/Maurel/Scripts/Launcher.cs
+++ Unity/Assets/Maurel/Scripts/Launcher.cs
@@ -2,9 +2,26 @@
 
 namespace Maurel.Scripts
 {
-    public class Launcher : MonoBehaviour
+    public class Launcher : Photon.PunBehaviour
     {
         #region Public Variables
+
+        /// <summary>
+        /// The maximum number of players per room.
+        /// </summary>   
+        [Tooltip(
+            "The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        public byte maxPlayersPerRoom = 4;
+        
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        public GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        public GameObject progressLabel;
+
+        /// <summary>
+        /// The PUN loglevel. 
+        /// </summary>
+        public PhotonLogLevel loglevel = PhotonLogLevel.Informational;
 
         #endregion
 
@@ -13,7 +30,7 @@ namespace Maurel.Scripts
         /// <summary>
         /// This client's version number. Users are separated from each other by gameversion.
         /// </summary>
-        private string _gameVersion = "0.1";
+        private const string GameVersion = "0.1";
 
         #endregion
 
@@ -21,18 +38,53 @@ namespace Maurel.Scripts
 
         private void Awake()
         {
-            // #Critical
-            // We don't join the lobby. There is no need to join a lobby to get the list of rooms.
+            // #Critical: We don't join the lobby. There is no need to join a lobby to get the list of rooms.
             PhotonNetwork.autoJoinLobby = false;
 
-            // #Critical
-            // This makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automaticaly
+            // #Critical: All clients in the same room sync their level automaticaly.
             PhotonNetwork.automaticallySyncScene = true;
+
+            // #NotImportant: Force LogLevel
+            PhotonNetwork.logLevel = loglevel;
         }
 
         private void Start()
         {
-            Connect();
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        #endregion
+
+        #region Photon.PunBehaviour Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+            
+            // #Critical: We try to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()  
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+            Debug.Log(
+                "DemoAnimator/Launcher: OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.");
+            
+            // #Critical: We failed to join a random room, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions {MaxPlayers = maxPlayersPerRoom}, null);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
         }
 
         #endregion
@@ -46,6 +98,9 @@ namespace Maurel.Scripts
         /// </summary>
         public void Connect()
         {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+            
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.connected)
             {
@@ -55,7 +110,7 @@ namespace Maurel.Scripts
             else
             {
                 // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+                PhotonNetwork.ConnectUsingSettings(GameVersion);
             }
         }
 
---
+++ Unity/Assets/Maurel/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Maurel.Scripts
+{
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        #region Private Variables
+
+        // Store the PlayerPref Key to avoid typos
+        static string playerNamePrefKey = "PlayerName";
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        private void Start()
+        {
+            string defaultName = "";
+            InputField inputField = GetComponent<InputField>();
+            if (inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    inputField.text = defaultName;
+                }
+            }
+
+
+            PhotonNetwork.playerName = defaultName;
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+        /// </summary>
+        /// <param name="value">The name of the Player</param>
+        public void SetPlayerName(string value)
+        {
+            // #Important: Force a trailing space string in case value is an empty string, else playerName would not be updated.
+            PhotonNetwork.playerName = value + " ";
+
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+
+        #endregion
+    }
+}
