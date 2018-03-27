c3d470277cf5b1722f26cd0b31c7f99c7aca973c
Rakotoasimbola <manasseh.johannes@gmail.com>
2018-03-27 21:12:48 +0200

    animation + prefab

---
+++ Unity/Assets/BattleRobo/Scripts/Camera_DepthTextureMode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Camera_DepthTextureMode : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    private void Start()
+    {
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
