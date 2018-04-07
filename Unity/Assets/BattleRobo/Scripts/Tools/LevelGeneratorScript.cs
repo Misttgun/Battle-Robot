11cf857ea6cf281be469e0a1890b09556e1edaf3
Rakotoasimbola <manasseh.johannes@gmail.com>
2018-04-07 22:31:39 +0200

    zone alpha

--- Unity/Assets/BattleRobo/Scripts/Tools/LevelGeneratorScript.cs
+++ Unity/Assets/BattleRobo/Scripts/Tools/LevelGeneratorScript.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace BattleRobo.Networking
 {
     public class LevelGeneratorScript : Photon.PunBehaviour
     {
         [SerializeField] private GameObject[] prefabsToLoad;
+        [SerializeField] private GameObject EnvExt;
         [SerializeField] private Transform level;
+        [SerializeField] private float Height;
         [SerializeField] private int mapSize;
-
+        [SerializeField] private int mapSpacing=6;
+        
         private void Start()
         {
+            
 			//On doit avoir le même seed sinon la map est différente pour les joueurs :D
-            Random.InitState(40);
-
+            Random.InitState(40); //seed
+            
             for (int i = 0; i < mapSize; i++)
             {
                 for (int j = 0; j < mapSize; j++)
@@ -20,12 +25,28 @@ namespace BattleRobo.Networking
                     var randomNum = Random.Range(1, prefabsToLoad.Length) - 1;
 
                     var platform = Instantiate(prefabsToLoad[randomNum],
-                        new Vector3(j * 6, prefabsToLoad[randomNum].transform.position.y, i * 6),
+                        new Vector3(j * mapSpacing, prefabsToLoad[randomNum].transform.position.y, i * mapSpacing),
                         Quaternion.identity);
                     platform.transform.Rotate(platform.transform.rotation.x, Random.Range(0,3)*90 , platform.transform.rotation.z);
                     platform.transform.parent = level;
                 }
             }
+            
+            int m = mapSize * mapSpacing;
+            EnvExt.transform.localScale= new Vector3(m,Height/4,m);
+            EnvExt.transform.position= new Vector3(m/2,Height/8,m/2);
+        }
+
+        public int getMapMainSize()
+        {
+            return mapSize*mapSpacing;
+        }
+
+        public float getHeight()
+        {
+            return Height;
         }
     }
+    
+    
 }
---
+++ Unity/Assets/Gameplay/Scripts/StormPlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormPlayerController : MonoBehaviour
+{
+    [SerializeField] private int life=10;
+    [SerializeField] private bool InTheStorm;
+    
+    public void StormApplyDmg(int Dmg)
+    {
+        StartCoroutine(StormManageDmg(Dmg));
+    }
+
+    public void StormStopDmg()
+    {
+        InTheStorm = false;
+    }
+
+    IEnumerator StormManageDmg(int Dmg)
+    {
+        InTheStorm = true;
+        while (InTheStorm)
+        {
+            yield return new WaitForSeconds(1f);
+            life-=Dmg;
+            if (life <= 0)
+            {
+                life = 0;
+                InTheStorm = false;
+            } 
+        }
+    }
+
+}
