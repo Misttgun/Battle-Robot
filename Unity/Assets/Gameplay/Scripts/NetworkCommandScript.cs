a71d8bad5ec40b1c0713dcc3152213a9f342c8e1
Misttgun <maurelarchange@gmail.com>
2018-04-27 17:12:47 +0200

    Serveur Autoritaire V1 Fonctionnel

---
+++ Unity/Assets/Gameplay/Scripts/NetworkCommandScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon;
+using UnityEngine;
+
+public class NetworkCommandScript : PunBehaviour
+{
+    [SerializeField] private CommandDispatcherScript commandDispatcherScript;
+
+    private const int IdShift = 1;
+
+    public PlayerController.PlayerState playerState = new PlayerController.PlayerState();
+    public PlayerController.PlayerState previousPlayerState = new PlayerController.PlayerState();
+
+    private void Start()
+    {
+        PhotonNetwork.RPC(photonView,
+            "SetUpRPC",
+            PhotonNetwork.player,
+            false,
+            PhotonNetwork.player);
+    }
+
+    private void Update()
+    {
+        // Cursor lock
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        Vector3 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        bool isJumping = Input.GetButton("Jump");
+
+        playerState = new PlayerController.PlayerState(inputX, inputY, isJumping, mouseInput);
+
+        if (!playerState.IsEqual(previousPlayerState))
+        {
+            PhotonNetwork.RPC(photonView,
+                "MovementRPC",
+                PhotonTargets.MasterClient,
+                false,
+                PhotonNetwork.player,
+                playerState.mouseInput,
+                playerState.inputX,
+                playerState.inputY,
+                playerState.isJumping);
+
+            previousPlayerState = playerState;
+        }
+    }
+
+    [PunRPC]
+    public void MovementRPC(PhotonPlayer player, Vector2 mouseInput, float inputX, float inputY, bool isJumping)
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            commandDispatcherScript.Movement(player.ID - IdShift, mouseInput, inputX, inputY, isJumping);
+        }
+    }
+
+    [PunRPC]
+    public void SetUpRPC(PhotonPlayer player)
+    {
+        commandDispatcherScript.SetUp(player.ID - IdShift);
+    }
+}
--- Unity/Assets/Gameplay/Scripts/PlayerController.cs
+++ Unity/Assets/Gameplay/Scripts/PlayerController.cs
@@ -1,53 +1,42 @@
 ﻿using System;
-using Photon;
 using UnityEngine;
 
 
-public class PlayerController : PunBehaviour
+public class PlayerController : Photon.MonoBehaviour, IPunObservable
 {
-    [Header("Mouse Settings")]
-    [SerializeField]
-    private float aimSensitivity = 5f;
+    [Header("Mouse Settings")] [SerializeField] private float aimSensitivity = 5f;
 
-    [Header("Movement Settings")]
-    [SerializeField]
-    private float walkSpeed = 6.0f;
+    [Header("Movement Settings")] [SerializeField] private float walkSpeed = 6.0f;
 
-    [SerializeField]
-    private float runSpeed = 11.0f;
+    [SerializeField] private float runSpeed = 11.0f;
 
-    [SerializeField]
-    private float gravity = 20.0f;
+    [SerializeField] private float gravity = 20.0f;
 
-    [SerializeField]
-    private float fuelDecreaseSpeed = 0.1f;
+    [SerializeField] private float fuelDecreaseSpeed = 0.1f;
 
-    [SerializeField]
-    private float fuelRegenSpeed = 0.05f;
+    [SerializeField] private float fuelRegenSpeed = 0.05f;
 
-    [SerializeField]
-    private float flyForce = 100f;
+    [SerializeField] private float flyForce = 100f;
 
-    [Header("Required Components")]
-    [SerializeField]
-    private CharacterController controller;
+    [Header("Required Components")] [SerializeField] private CharacterController controller;
+
+    [SerializeField] private Transform camTransform;
 
     /// <summary>
     /// The camera gameObject for the shooting.
     /// </summary>
-    [SerializeField]
-    private GameObject playerCamera;
-
-    [SerializeField]
-    private PhotonView myPhotonView;
+    [SerializeField] private GameObject playerCamera;
 
     private Vector3 moveDirection = Vector3.zero;
     private bool grounded;
     private float speed;
+    private Vector3 fly;
 
     public float fuelAmount = 1f;
     private float maxFuelAmount = 1f;
     public float currentRot;
+
+    public PlayerState playerState = new PlayerState();
 
     private Transform myTransform;
 
@@ -59,114 +48,58 @@ public class PlayerController : PunBehaviour
         myTransform = transform;
         speed = walkSpeed;
 
-        if (!myPhotonView.isMine)
-            return;
-
-        //activate camera only for this player
-        playerCamera.SetActive(true);
+        PhotonNetwork.sendRate = 30;
+        PhotonNetwork.sendRateOnSerialize = 30;
+    }
+    
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(currentRot);
+        }
+        else
+        {
+            currentRot = (float)stream.ReceiveNext();
+        }
     }
 
     private void FixedUpdate()
     {
-        if (!myPhotonView.isMine)
-            return;
-
-        // Rotate the player on the Y axis
-        myTransform.rotation *= Quaternion.Euler(0, Input.GetAxisRaw("Mouse X") * aimSensitivity, 0);
-    }
-
-    private void Update()
-    {
-        if (!myPhotonView.isMine)
-            return;
-
-        // Cursor lock
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.L))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        float consumedFuel = 0f;
-
-        if (Input.GetButton("Jump") && fuelAmount > 0f)
-        {
-            fuelAmount -= fuelDecreaseSpeed * Time.deltaTime;
-            consumedFuel = maxFuelAmount - fuelAmount;
-        }
-        else
-        {
-            fuelAmount += fuelRegenSpeed * Time.deltaTime;
-            maxFuelAmount = fuelAmount;
-        }
-
-        myPhotonView.RPC("ClientMovement", PhotonTargets.All, inputX, inputY, consumedFuel, fuelAmount);
-
-        fuelAmount = Mathf.Clamp(fuelAmount, 0f, 1f);
-    }
-
-    private void LateUpdate()
-    {
-        if (!myPhotonView.isMine)
-            return;
-
-        // Rotate the player on the X axis
-        currentRot -= Input.GetAxisRaw("Mouse Y") * aimSensitivity;
-        currentRot = Mathf.Clamp(currentRot, -60f, 60f);
-    }
-
-    private void Jump(float fuel, float consFuel)
-    {
-        Vector3 fly = Vector3.zero;
-        if (fuel >= 0.15f)
-        {
-            fly = Vector3.up * flyForce * consFuel;
-        }
-
-        if (fly != Vector3.zero)
-        {
-            moveDirection.y = fly.y;
-        }
-    }
-
-    [PunRPC]
-    public void ClientMovement(float inputX, float inputY, float consumedFuel, float fuel)
-    {
-        if (!myPhotonView.isMine)
-            return;
-
         // Limit the diagonal speed
-        float inputModifyFactor = Math.Abs(inputX) > 0.0001f && Math.Abs(inputY) > 0.0001f ? .7071f : 1.0f;
+        float inputModifyFactor = Math.Abs(playerState.inputX) > 0.0001f && Math.Abs(playerState.inputY) > 0.0001f
+            ? .7071f
+            : 1.0f;
 
         // Handle the movement
         if (grounded)
         {
             speed = Input.GetButton("Run") ? runSpeed : walkSpeed;
 
-            moveDirection = new Vector3(inputX * inputModifyFactor, 0f, inputY * inputModifyFactor);
+            moveDirection = new Vector3(playerState.inputX * inputModifyFactor, 0f,
+                playerState.inputY * inputModifyFactor);
             moveDirection = myTransform.TransformDirection(moveDirection) * speed;
 
             // Jump!
-            Jump(fuel, consumedFuel);
+            Jump();
         }
         else
         {
-            moveDirection.x = inputX * speed * inputModifyFactor;
-            moveDirection.z = inputY * speed * inputModifyFactor;
+            moveDirection.x = playerState.inputX * speed * inputModifyFactor;
+            moveDirection.z = playerState.inputY * speed * inputModifyFactor;
             moveDirection = myTransform.TransformDirection(moveDirection);
 
             // Jump!
-            Jump(fuel, consumedFuel);
+            Jump();
         }
 
+        // Rotate the player on the Y axis
+        myTransform.rotation *= Quaternion.Euler(0, playerState.mouseInput.x * aimSensitivity, 0);
+        
+        // Rotate the player on the X axis
+        currentRot -= playerState.mouseInput.y * aimSensitivity;
+        currentRot = Mathf.Clamp(currentRot, -60f, 60f);
+        
         // Apply gravity
         moveDirection.y -= gravity * Time.deltaTime;
 
@@ -174,28 +107,78 @@ public class PlayerController : PunBehaviour
         grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;
     }
 
+    private void Update()
+    {
+        fly = Vector3.zero;
+        if (playerState.isJumping && fuelAmount > 0f)
+        {
+            fuelAmount -= fuelDecreaseSpeed * Time.deltaTime;
+            var consumedFuel = maxFuelAmount - fuelAmount;
+
+            if (fuelAmount >= 0.15f)
+            {
+                fly = Vector3.up * flyForce * consumedFuel;
+            }
+        }
+        else
+        {
+            fuelAmount += fuelRegenSpeed * Time.deltaTime;
+            maxFuelAmount = fuelAmount;
+        }
+
+        fuelAmount = Mathf.Clamp(fuelAmount, 0f, 1f);
+    }
+
+    private void Jump()
+    {
+        if (fly != Vector3.zero)
+        {
+            moveDirection.y = fly.y;
+        }
+    }
+
+    public void ClientMovement(Vector2 mouseIn, float inputX, float inputY, bool isJumping)
+    {
+        playerState = new PlayerState(inputX, inputY, isJumping, mouseIn);
+    }
+
+    public void SetUp()
+    {
+        //activate camera only for this player
+        playerCamera.SetActive(true);
+    }
+
     [Serializable]
     public class PlayerState
     {
-        public float InputX { get; private set; }
+        public float inputX;
 
-        public float InputY { get; private set; }
+        public float inputY;
 
-        public float ConsumedFuel { get; private set; }
+        public bool isJumping;
 
-        public float Fuel { get; private set; }
+        public Vector2 mouseInput;
 
-        public PlayerState(float inputX, float inputY, float consumedFuel, float fuel)
+        public PlayerState()
         {
-            InputX = inputX;
-            InputY = inputY;
-            ConsumedFuel = consumedFuel;
-            Fuel = fuel;
+            inputX = 0f;
+            inputY = 0f;
+            isJumping = false;
+            mouseInput = Vector3.zero;
+        }
+
+        public PlayerState(float inputX, float inputY, bool isJumping, Vector2 mouseInput)
+        {
+            this.inputX = inputX;
+            this.inputY = inputY;
+            this.isJumping = isJumping;
+            this.mouseInput = mouseInput;
         }
 
         public bool IsEqual(PlayerState other)
         {
-            return InputX.Equals(other.InputX) && InputY.Equals(other.InputY) && ConsumedFuel.Equals(other.ConsumedFuel);
+            return inputX.Equals(other.inputX) && inputY.Equals(other.inputY) &&
+                   isJumping.Equals(other.isJumping) && mouseInput == other.mouseInput;
         }
     }
 }
