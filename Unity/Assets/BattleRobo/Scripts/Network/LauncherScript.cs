73eb07e60a4778ba97c9e6e736ae9336cc0d72e7
Vincent Girardot <girardot.vin@gmail.com>
2018-06-17 12:24:42 +0200

    Mise à jour de l'UI du launcher pour la connexion

--- Unity/Assets/BattleRobo/Scripts/Network/LauncherScript.cs
+++ Unity/Assets/BattleRobo/Scripts/Network/LauncherScript.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace BattleRobo.Networking
 {
@@ -9,6 +12,12 @@ namespace BattleRobo.Networking
 
         // The UI Label to inform the user that the connection is in progress
         public GameObject progressLabel;
+        
+        // The UI Panl to let the user create an account
+        public GameObject createPanel;
+        
+        // The UI error Pannel
+        public GameObject errorPanel;
 
 		/// <summary>
 		/// Game version. Only players with the same version will find each other.
@@ -42,13 +51,54 @@ namespace BattleRobo.Networking
         /// - Esle, connect this application instance to Photon Cloud Network
         /// </summary>
         public void Connect()
-        {
+        {            
             progressLabel.SetActive(true);
             controlPanel.SetActive(false);
 
 			// We try to connect to server
 			PhotonNetwork.PhotonServerSettings.HostType = ServerSettings.HostingOption.BestRegion;
 			PhotonNetwork.ConnectToBestCloudServer(GameVersion);
+        }
+
+        public void Create()
+        {
+            string url = "http://51.38.235.234:8080/add_player?pseudo=Kas&pass=password";
+
+            WWW www = new WWW(url);
+            
+            // - wait response
+            while (!www.isDone);
+            
+            Debug.Log(" Test : " + (String)www.text + " " + www.responseHeaders["STATUS"]);
+        }
+
+        public void GoToCreatePanel()
+        {
+            controlPanel.SetActive(false);
+            createPanel.SetActive(true);
+        }
+
+        public void GoToErrorPanel(string error)
+        {
+            errorPanel.SetActive(true);
+        }
+
+        public void GoToConnectPanel()
+        {
+            createPanel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        public void AuthentificationFailed()
+        {
+            controlPanel.SetActive(false);
+            GoToErrorPanel("Authentification failed");
+        }
+
+        public void UsernameExist()
+        {
+            createPanel.SetActive(false);
+            GoToErrorPanel("Username already exist");
         }
     }
 }
