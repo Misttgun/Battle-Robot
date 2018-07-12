c3d54e3ffcb7b161f030f434210cc3e516be7bbb
Kasparrow <girardot.vin@gmail.com>
2018-07-12 12:37:27 +0200

    Add https support

--- Unity/Assets/BattleRobo/Scripts/Utils/DatabaseRequester.cs
+++ Unity/Assets/BattleRobo/Scripts/Utils/DatabaseRequester.cs
@@ -1,6 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
 
 namespace BattleRobo
 {
@@ -30,6 +36,7 @@ namespace BattleRobo
             DontDestroyOnLoad(gameObject);
         }
 
+        /*
         public void AsyncQuery(string query)
         {
             // - create query (use stringBuilder ?)
@@ -38,8 +45,9 @@ namespace BattleRobo
 
             // - don't wait for response
             WWW www = new WWW(url);
-        }
+        }*/
 
+        /*
         public void SyncQuery(string query, out int status, out string res)
         {
             // - create query (use stringBuilder ?)
@@ -59,6 +67,47 @@ namespace BattleRobo
                 status = 400;
 
             res = www.text;
+        }*/
+
+        public void AsyncQuery(string query)
+        {
+            // - create query (use stringBuilder ?)
+            string url = useHttps ? "https://" : "http://";
+            url += ip + ":" + port + "/" + query;
+
+            // - don't wait for response
+            ServicePointManager.ServerCertificateValidationCallback = TrustCertificate;
+ 
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            
+            // - fire and forget pattern
+            ThreadPool.QueueUserWorkItem(o=>{ request.GetResponse(); });
+            
+        }
+
+        public void SyncQuery(string query, out int status, out string res)
+        {
+            string url = useHttps ? "https://" : "http://";
+            url += ip + ":" + port + "/" + query;
+            
+            ServicePointManager.ServerCertificateValidationCallback = TrustCertificate;
+ 
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+            Stream dataStream = response.GetResponseStream ();
+            StreamReader reader = new StreamReader (dataStream);
+            string responseFromServer = reader.ReadToEnd ();
+ 
+            status = (int) response.StatusCode;
+            res = responseFromServer;
+            Debug.Log("STATUS : " + status + " - " + res + "URL USED : " + url);
+        }
+        
+        private static bool TrustCertificate(object sender, X509Certificate x509Certificate, X509Chain x509Chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // all Certificates are accepted
+            return true;
         }
 
         public void EnableAuthenticationServer(bool enable)
